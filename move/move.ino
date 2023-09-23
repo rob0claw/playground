87b8e7bfceea8cb9e73d998803fea1db4f6118d3
TheParadox20 <onyambusamson@gmail.com>
2023-09-23 16:46:45 +0300

    move motor move

---
+++ move/move.ino
@@ -0,0 +1,19 @@
+int IN1=8;
+int IN2=9;
+int ENA=3;
+void setup()
+{
+ pinMode(IN1,OUTPUT);
+ pinMode(IN2,OUTPUT);
+ pinMode(ENA,OUTPUT);
+}
+void loop()
+{  
+ analogWrite(ENA, 200);// motor speed  
+ digitalWrite(IN1,LOW);// rotate forward
+ digitalWrite(IN2,HIGH);
+ delay(2000);  
+ digitalWrite(IN1,HIGH);// rotate reverse
+ digitalWrite(IN2,LOW);
+ delay(2000);
+}
